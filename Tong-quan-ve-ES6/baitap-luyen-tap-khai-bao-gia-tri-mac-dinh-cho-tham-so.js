37c58a528827f148b2ac201281239477b0bff6c9
nguyenmihduc <106855518+nguyenmihduc@users.noreply.github.com>
2022-07-03 14:47:04 +0700

    cap nhat

---
+++ Tong-quan-ve-ES6/baitap-luyen-tap-khai-bao-gia-tri-mac-dinh-cho-tham-so.js
@@ -0,0 +1,9 @@
+
+function lastIndexOf(arr, elt, start = 3) {
+    for (let i = start - 1; i >= 0; i--)
+        if (arr[i] === elt) {
+            return i
+        }
+    return -1
+}
+console.log(lastIndexOf([1, 4, 1, 4], 4))
